83ea8dd575ed7475e8196c6c999cf49701e92224
unknown <eseanu.cristian@gmail.com>
2017-11-07 22:00:25 +0200

    innerHTML protection - sanitize input

---
+++ xss/innerHTML/target/index.php
@@ -0,0 +1,31 @@
+<html>
+	<body>
+		<h1>Login Page</h1>
+
+		<form action="testify.php" onsubmit="sanitize()" method="post">
+			 <table>
+    			<tr>
+      				<td align="left">Username:</td>
+      				<td align="center"><input type="text" id="username" name="user"></td>
+      			</tr>
+
+            <tr>
+        				<td align="left">Password:</td>
+        				<td align="center"><input type="text" name="name"></td>
+        			</tr>
+    		</table>
+			<br><input type="submit">
+		</form>
+		<script>
+			alert("running")
+		</script>
+		<script>
+			function sanitize() {
+					/*alert("asdasd");*/
+					var x = document.getElementById("user").value;
+					document.getElementById("username").innerHTML = x;
+					/*alert(document.getElementById("username"));*/
+			}
+		</script>
+	</body>
+</html>
